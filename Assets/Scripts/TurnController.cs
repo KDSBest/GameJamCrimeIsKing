20d56d1a761f395f207671ea42342954ae101eae
ofm-adrian-loew <adrian.loew@onlinefussballmanager-gmbh.de>
2016-11-03 22:49:48 +0100

    turn controller

---
+++ Assets/Scripts/TurnController.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnController : MonoBehaviour
+{
+    public KingController KingController;
+
+    public CriminalController CriminalController;
+
+    public Button EndTurnButton;
+
+    public Text CurrentTurnDisplayText;
+
+    public GameObject BlackOverlay;
+
+    public Button StartTurnButton;
+
+    public enum Turn
+    {
+        King,
+        Criminal
+    }
+
+    public Turn CurrenTurn;
+
+    // Use this for initialization
+    public void Start()
+    {
+        this.CurrenTurn = Turn.King;
+
+        this.EndTurnButton.onClick.RemoveAllListeners();
+        this.EndTurnButton.onClick.AddListener(this.EndCurrentTurn);
+
+        this.StartTurnButton.onClick.RemoveAllListeners();
+        this.StartTurnButton.onClick.AddListener(this.StartTurn);
+    }
+
+    public void StartTurn()
+    {
+        this.BlackOverlay.SetActive(false);
+        this.StartTurnButton.gameObject.SetActive(false);
+    }
+
+    public void EndCurrentTurn()
+    {
+        this.CurrenTurn = this.CurrenTurn == Turn.King ? Turn.Criminal : Turn.King;
+        this.CurrentTurnDisplayText.text = this.CurrenTurn.ToString();
+        this.BlackOverlay.SetActive(true);
+        this.StartTurnButton.gameObject.SetActive(true);
+
+        switch (this.CurrenTurn)
+        {
+            case Turn.King:
+                this.CriminalController.EndTurn();
+                this.KingController.StartTurn();
+                break;
+            case Turn.Criminal:
+                this.KingController.EndTurn();
+                this.CriminalController.StartTurn();
+                break;
+        }
+    }
+}
