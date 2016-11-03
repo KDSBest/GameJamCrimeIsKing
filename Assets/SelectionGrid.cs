eba1f135858f373cf9123a5cf7672df9e9bdcd62
Kevin Setiono <kevin.setiono@onlinefussballmanager-gmbh.de>
2016-11-03 21:35:37 +0100

    Initial Commit

---
+++ Assets/SelectionGrid.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectionGrid : MonoBehaviour
+{
+    public GameObject SelectionOverlay;
+
+    public void OnMouseOver()
+    {
+        var mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(mouseRay, out hit))
+        {
+            this.SelectionOverlay.transform.position = new Vector3(Mathf.Round(hit.point.x), this.SelectionOverlay.transform.position.y, Mathf.Round(hit.point.z));
+        }
+    }
+}
